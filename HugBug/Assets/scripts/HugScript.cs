7a96e2e8a3f1089428ef2a5160b84aad4662c1ff
FunnyGoodHeart <147742076+FunnyGoodHeart@users.noreply.github.com>
2024-02-27 09:24:30 -0800

    fixed movement

--- HugBug/Assets/scripts/HugScript.cs
+++ HugBug/Assets/scripts/HugScript.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class HugScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] TextMeshProUGUI hugCounterText;
+
+    [SerializeField]  int hugCount = 0;
+    GameObject nearbyFriend;
+    bool CloseEnough = false;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision Enter");
+        if(gameObject.gameObject.tag == "Friend")
+        {
+            //nearbyFriend = collision.gameObject;
+            CloseEnough = true;
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Collision Exit");
+        if (collision.gameObject.tag == "Friend")
+        {
+            CloseEnough = false;
+        }
+    }
+    void OnHug()
+    {
+        Debug.Log("Hug");
+        if (CloseEnough == true)
+        {
+            Debug.Log("Hug pt.2");
+            hugCount++;
+            hugCounterText.text = "Hugs Given: " + hugCount;
+        }
     }
 }
